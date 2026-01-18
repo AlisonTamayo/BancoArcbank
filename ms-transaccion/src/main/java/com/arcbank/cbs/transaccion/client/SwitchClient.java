1f6a4acea6218c18926cee5248ba4a0c3eba0fa5
Alison Tamayo <aatamayo2@espe.edu.ec>
2026-01-18 02:22:40 -0500

    comunicacion bancos

--- ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/client/SwitchClient.java
+++ ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/client/SwitchClient.java
@@ -15,7 +15,7 @@ import com.arcbank.cbs.transaccion.dto.SwitchTransferResponse;
 @FeignClient(name = "digiconecu-switch", url = "${app.switch.network-url:https://switch-digiconecu.duckdns.org}", configuration = com.arcbank.cbs.transaccion.config.MTLSConfig.class)
 public interface SwitchClient {
 
-    @PostMapping("/api/v1/transacciones")
+    @PostMapping("/api/v2/switch/transfers")
     String enviarTransferencia(@RequestBody SwitchTransferRequest request);
 
     @GetMapping("/api/v1/red/bancos")
@@ -24,6 +24,6 @@ public interface SwitchClient {
     @GetMapping("/api/v2/transfers/health")
     Map<String, String> healthCheck();
 
-    @PostMapping("/api/v1/transacciones/devoluciones")
+    @PostMapping("/api/v2/switch/transfers/return")
     String enviarDevolucion(@RequestBody com.arcbank.cbs.transaccion.dto.SwitchDevolucionRequest request);
 }
