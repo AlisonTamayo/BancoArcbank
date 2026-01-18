ae0b64b603f0c4aee5664f8835ff391068aa61ce
Alison Tamayo <aatamayo2@espe.edu.ec>
2026-01-17 22:08:24 -0500

    inicio sesion arreglar

--- ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/controller/WebhookController.java
+++ ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/controller/WebhookController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arcbank.cbs.transaccion.dto.SwitchTransferRequest;
